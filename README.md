f5fd4eedbc937f50d7cdc53992d023213d86cf7e
gestionarlaweb <gestionarlaweb@gmail.com>
2021-07-13 23:27:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# datepicker
+
+Flutter project.
+
+## Ejemple de DatePicker
+
+Cupertino si detecta iOs y Material si detecta Android
---
+++ android/app/src/main/kotlin/com/gestionarlaweb/datepicker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gestionarlaweb.datepicker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
